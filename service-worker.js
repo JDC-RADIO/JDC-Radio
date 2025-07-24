7a2df4a2638e7eb5328c0a4b021b434d218447a4
clcproducciones <149100918+clcproducciones@users.noreply.github.com>
2025-07-24 16:00:48 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -3,10 +3,10 @@ self.addEventListener('install', function(event) {
     event.waitUntil(
         caches.open('jdc-radio-v1').then(function(cache) {
             return cache.addAll([
-                '/',
-                '/index.html',
-                '/style.css',
-                '/app.js'
+                '/JDC-Radio/',
+                '/JDC-Radio/index.html',
+                '/JDC-Radio/style.css',
+                '/JDC-Radio/app.js'
             ]);
         })
     );
